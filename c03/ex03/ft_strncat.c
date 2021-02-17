41bc0542265d0f347b778afaa4fa1ff45b1d4381
Jonathan Duong <ijoe@Mac-mini-de-Jonathan.local>
2021-02-17 21:57:36 +0100

    100%

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joduong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 12:34:38 by joduong           #+#    #+#             */
+/*   Updated: 2020/07/09 18:49:43 by joduong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int				i;
+	unsigned int	j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] && j < nb)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ c03/ex05/ft_strlcat.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joduong <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 15:22:23 by joduong           #+#    #+#             */
+/*   Updated: 2020/07/18 09:49:11 by joduong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	r;
+
+	i = 0;
+	j = 0;
+	r = 0;
+	while (dest[i] && src[r])
+	{
+		i++;
+		r++;
+	}
+	if (size <= i)
+		r += size;
+	else
+		r += i;
+	while (src[j] && i + 1 < size)
+		dest[i++] = src[j++];
+	dest[i] = '\0';
+	return (r);
+}
